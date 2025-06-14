4efa805bc05c2f92154f4a90e2a15ff13f8821ed
Tali <talicabana@gmail.com>
2025-06-14 13:25:23 -0300

    Mostramos por consola variables

---
+++ js/01-variables.js
@@ -0,0 +1,17 @@
+// Este es un comentario en una sola linea
+
+/* 
+Esto es un comentario
+de múltiples
+líneas
+*/
+
+// crear una variable (let - const - var)
+
+let numero1;
+
+let anio = 2025;
+
+// Mostrar valor de variables
+console.log("Hola mundo")
+console.log(numero1, anio)
