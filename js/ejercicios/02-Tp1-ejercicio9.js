80cf249014ef83c1379aefdf7c9301b44f2d073c
Tali <talicabana@gmail.com>
2025-06-15 18:44:27 -0300

    Aprendimos qué hace length, substing y charAt

--- js/ejercicios/02-Tp1-ejercicio9.js
+++ js/ejercicios/02-Tp1-ejercicio9.js
@@ -1,3 +1,9 @@
 // 9- Escribe un programa que pida una frase y escriba las vocales que aparecen.
 // Nota: Tener en cuenta la funcion length y substring o charAt (Developer Mozilla)
 
+let frase = prompt ("Ingresá una frase")
+// frase de ejemplo: hola mundo
+console.log(frase.length)
+console.log(frase.substring(0,3))
+console.log(frase.charAt(0))
+console.log(frase.charAt(5))
